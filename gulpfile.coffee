0487d88c42a5f94b26f578227e9ea26afd34ccc0
Iain Houston <mail@iainhouston.me>
2018-03-06 10:14:07 +0000

    Added gulpfile

---
+++ gulpfile.coffee
@@ -0,0 +1,49 @@
+# project specific settings
+drushAlias      = '@misdev8'
+test_site_name  = 'vagrant.misfitstheatre.test'
+
+cssSources   = [ 'sourcecss/**/*.css' ]
+drupalPHPSources = [ '**/*.{php,inc}' ]
+drupalTemplateSources = [ '**/*.html.twig' ]
+
+# workflow process settings
+gulp = require('gulp')
+shell = require('gulp-shell')
+postcss = require('gulp-postcss')
+browserSync = require('browser-sync').create()
+reload = browserSync.reload
+
+
+gulp.task 'drushPHP', shell.task([ "drush #{drushAlias} cr" ])
+
+gulp.task 'drushTwig', shell.task([ "drush #{drushAlias} cache-clear theme-registry" ])
+
+gulp.task 'css', ->
+  gulp.src('css/style.css')
+  .pipe(postcss([
+    require('postcss-import')()
+    require('postcss-url')()
+    require('postcss-cssnext')()
+    require('postcss-browser-reporter')()
+    require('postcss-reporter')()
+  ]))
+  .pipe(gulp.dest('./postcss'))
+  .pipe reload(stream: true)
+  return
+
+gulp.task 'watch-server', [
+  'css'
+  'drushPHP'
+  'drushTwig'
+], ->
+  browserSync.init
+    proxy: test_site_name
+    reloadOnRestart: true
+    browser: '/Applications/FirefoxDeveloperEdition.app'
+  gulp.watch cssSources,    [ 'css' ]
+  gulp.watch drupalSources, [ 'drushPHP' ]
+  gulp.watch drupalSources, [ 'drushTwig' ], reload
+  return
+
+# Default task to be run with `gulp`
+gulp.task 'default', [ 'watch-server' ]
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var browserSync, cssSources, drupalPHPSources, drupalTemplateSources, drushAlias, gulp, postcss, reload, shell, test_site_name;
+
+drushAlias = '@misdev8';
+
+test_site_name = 'vagrant.misfitstheatre.test';
+
+cssSources = ['sourcecss/**/*.css'];
+
+drupalPHPSources = ['**/*.{php,inc}'];
+
+drupalTemplateSources = ['**/*.html.twig'];
+
+gulp = require('gulp');
+
+shell = require('gulp-shell');
+
+postcss = require('gulp-postcss');
+
+browserSync = require('browser-sync').create();
+
+reload = browserSync.reload;
+
+gulp.task('drushPHP', shell.task(["drush " + drushAlias + " cr"]));
+
+gulp.task('drushTwig', shell.task(["drush " + drushAlias + " cache-clear theme-registry"]));
+
+gulp.task('css', function() {
+  gulp.src('css/style.css').pipe(postcss([require('postcss-import')(), require('postcss-url')(), require('postcss-cssnext')(), require('postcss-browser-reporter')(), require('postcss-reporter')()])).pipe(gulp.dest('./postcss')).pipe(reload({
+    stream: true
+  }));
+});
+
+gulp.task('watch-server', ['css', 'drushPHP', 'drushTwig'], function() {
+  browserSync.init({
+    proxy: test_site_name,
+    reloadOnRestart: true,
+    browser: '/Applications/FirefoxDeveloperEdition.app'
+  });
+  gulp.watch(cssSources, ['css']);
+  gulp.watch(drupalSources, ['drushPHP']);
+  gulp.watch(drupalSources, ['drushTwig'], reload);
+});
+
+gulp.task('default', ['watch-server']);
