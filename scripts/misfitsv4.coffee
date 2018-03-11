cc845782615a8f22082bb072ba63059bb8229e5e
Iain Houston <mail@iainhouston.me>
2018-03-11 01:08:51 +0000

    Thumbscrubber working - on each instance

--- scripts/misfitsv4.coffee
+++ scripts/misfitsv4.coffee
@@ -1,14 +1,37 @@
 (($, Drupal) ->
-  Drupal.behaviors.myModuleBehavior = attach: (context, settings) ->
-    $('div.thumbscrubber', context).once('thumbscrubber').each ->
+  Drupal.behaviors.misfitsv4ThumbScrubber = attach: (context, settings) ->
+    $('div.thumbscrubber', context).once('init').each ->
       x = undefined
       ts_prevslide = undefined
       ts_currslide = undefined
       $this = $(this)
+      $this.addClass 'thumbs-init'
       width = $(this).innerWidth()
       $slides = $('.ts-inner', this).children()
       numslides = $slides.length
       console.log numslides
+      # Make the first photo opaque
+      $('.ts-inner > :first-child').addClass 'ts-currslide'
+      # listen for mousemove
+      $this.mousemove (event) ->
+        x = event.pageX - ($this.offset().left)
+        ts_currslide = Math.floor(x / (width / numslides)) + 1
+        # don't add/remove class if mouse inside current slide threshold
+        if ts_currslide != ts_prevslide
+          ts_prevslide = ts_currslide
+          $('.ts-inner > .ts-currslide', this).removeClass 'ts-currslide'
+          $('.ts-inner > :nth-child(' + ts_currslide + ')', this).addClass 'ts-currslide'
+        false
+      # mousemove
+      # on mouseleave, reset back to first slide
+      $this.mouseleave (event) ->
+        if ts_currslide != 1
+          ts_currslide = 1
+          ts_prevslide = 1
+          $('.ts-inner > .ts-currslide', this).removeClass 'ts-currslide'
+          $('.ts-inner > :nth-child(' + ts_currslide + ')', this).addClass 'ts-currslide'
+        false
+        # mouseleave      
       return
     return
   return
--- scripts/misfitsv4.js
+++ scripts/misfitsv4.js
@@ -1,16 +1,37 @@
 (function($, Drupal) {
-  Drupal.behaviors.myModuleBehavior = {
+  Drupal.behaviors.misfitsv4ThumbScrubber = {
     attach: function(context, settings) {
-      $('div.thumbscrubber', context).once('thumbscrubber').each(function() {
+      $('div.thumbscrubber', context).once('init').each(function() {
         var $slides, $this, numslides, ts_currslide, ts_prevslide, width, x;
         x = void 0;
         ts_prevslide = void 0;
         ts_currslide = void 0;
         $this = $(this);
+        $this.addClass('thumbs-init');
         width = $(this).innerWidth();
         $slides = $('.ts-inner', this).children();
         numslides = $slides.length;
         console.log(numslides);
+        $('.ts-inner > :first-child').addClass('ts-currslide');
+        $this.mousemove(function(event) {
+          x = event.pageX - ($this.offset().left);
+          ts_currslide = Math.floor(x / (width / numslides)) + 1;
+          if (ts_currslide !== ts_prevslide) {
+            ts_prevslide = ts_currslide;
+            $('.ts-inner > .ts-currslide', this).removeClass('ts-currslide');
+            $('.ts-inner > :nth-child(' + ts_currslide + ')', this).addClass('ts-currslide');
+          }
+          return false;
+        });
+        $this.mouseleave(function(event) {
+          if (ts_currslide !== 1) {
+            ts_currslide = 1;
+            ts_prevslide = 1;
+            $('.ts-inner > .ts-currslide', this).removeClass('ts-currslide');
+            $('.ts-inner > :nth-child(' + ts_currslide + ')', this).addClass('ts-currslide');
+          }
+          return false;
+        });
       });
     }
   };
